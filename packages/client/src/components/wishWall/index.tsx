15a41a500ab1141caeac3ed509156ca870899019
k1ic <k1ic@k1icdeMacBook-Air.local>
2025-06-04 15:58:15 +0800

    create utils/propsData.ts

--- packages/client/src/components/wishWall/index.tsx
+++ packages/client/src/components/wishWall/index.tsx
@@ -6,6 +6,7 @@ import { encodeEntity, singletonEntity } from "@latticexyz/store-sync/recs";
 import { wishPool } from "../../utils/contants";
 import { pad } from "viem";
 import { format } from "date-fns";
+import { propsData } from "../../utils/propsData";
 
 interface WishInfo {
   wisher: string;
@@ -17,33 +18,6 @@ interface WishInfo {
 type PropsItem = {
   name: string;
   imageUrl: string;
-};
-
-const propsData: Record<number, PropsItem> = {
-  // Pray Blessing props
-  1: { name: "NAMASKAR", imageUrl: "images/wish/WishPanel/Props/NAMASKAR.png" },
-
-  // Health Blessing Props
-  2: { name: "Medicinal Herb Pouch", imageUrl: "images/wish/WishPanel/Props/2.1.png" },
-  3: { name: "Green Wellness Candle", imageUrl: "images/wish/WishPanel/Props/2.2.png" },
-  4: { name: "Horseshoe", imageUrl: "images/wish/WishPanel/Props/2.3.png" },
-  5: { name: "Red String", imageUrl: "images/wish/WishPanel/Props/2.4.png" },
-
-  // Fortune Blessing Props
-  6: { name: "Treasure Basin", imageUrl: "images/wish/WishPanel/Props/3.1.png" },
-  7: { name: "Lucky Beckoning Cat", imageUrl: "images/wish/WishPanel/Props/3.2.png" },
-  8: { name: "God of Wealth", imageUrl: "images/wish/WishPanel/Props/3.3.png" },
-  9: { name: "BitCoin", imageUrl: "images/wish/WishPanel/Props/3.4.png" },
-
-  // Wisdom Blessing Props
-  10: { name: "Koi Fish", imageUrl: "images/wish/WishPanel/Props/4.1.png" },
-  11: { name: "Athena's Owl", imageUrl: "images/wish/WishPanel/Props/4.2.png" },
-  12: { name: "Albert Einstein", imageUrl: "images/wish/WishPanel/Props/4.3.png" },
-
-  // Love Blessing Props
-  13: { name: "Cupid's Arrow", imageUrl: "images/wish/WishPanel/Props/1.1.png" },
-  14: { name: "Lucky Love Box", imageUrl: "images/wish/WishPanel/Props/1.2.png" },
-  15: { name: "Love Candle", imageUrl: "images/wish/WishPanel/Props/1.3.png" },
 };
 
 export default function WishingWall() {
