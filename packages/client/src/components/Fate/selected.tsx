e85316fccf76a7adb46dddd334b0830e83c33137
MJumpKing <2572430586@qq.com>
2025-06-13 18:21:09 +0800

    feat: add fated data for selected

--- packages/client/src/components/Fate/selected.tsx
+++ packages/client/src/components/Fate/selected.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './selected.module.css';
 import { shortenAddress } from '../../utils/common';
 import { useAccount } from 'wagmi';
-import { getBoostWisherRecords, getWisherCycleRecords } from '../common';
+import { getBoostWisherRecords, getCycleInfo, getWisherByIndex, getWisherCycleRecords } from '../common';
 import { formatEther } from 'viem';
 import { CURRENCY_SYMBOL } from "../../utils/contants"
 
@@ -29,34 +29,75 @@ const Selected = ({ cycle, onClose }: Props) => {
       return
     }
     const boostWisherRecords = getBoostWisherRecords(cycle);
-    if (!boostWisherRecords) {
-      return;
-    }
+    if (!boostWisherRecords) return;
+
+    const cycleInfo = getCycleInfo(cycle, 1);
+    if (!cycleInfo) return;
+    console.log(cycleInfo);
+    
+
+    const wisherStarArr: string[] =
+      boostWisherRecords.boostedWisherByStar.length === 0
+        ? getStarWisherArr()
+        : [];
+
     const tempData: WisherData[] = [];
-    for (let index = 0; index < boostWisherRecords.boostedWisherByPoints.length; index++) {
-      const wisher = boostWisherRecords.boostedWisherByPoints[index];
-      const boostedAmountData = getWisherCycleRecords(cycle, wisher);
-      if (!boostedAmountData) {
+    const indexId = Number(cycleInfo.wisherIndexId);
+    for (let index = 1; index <= cycleInfo.wisherCount; index++) {
+      const wisherInfo = getWisherByIndex(1, indexId, index);
+
+      if (!wisherInfo) {
+        continue;
+      }
+      const wisher = wisherInfo.wisher;
+      const wisherCycleRecord = getWisherCycleRecords(cycle, wisher);
+      if (!wisherCycleRecord) {
         continue;
       }
       tempData.push({
-        wisher,
-        wp: Number(boostedAmountData.points),
-        wp_pool_rewards: Number(formatEther(boostedAmountData.boostedPointsAmount)),
-        fated_pool_qualified: false,
-        fated_pool_rewards: Number(formatEther(0)),
+        wisher: wisher,
+        wp: Number(wisherCycleRecord.points),
+        wp_pool_rewards: Number(formatEther(wisherCycleRecord.boostedPointsAmount)),
+        fated_pool_qualified: boostWisherRecords.boostedWisherByStar.includes(wisher) || wisherStarArr.includes(wisher),
+        fated_pool_rewards: Number(formatEther(wisherCycleRecord.boostedStarAmount)),
       });
     }
-    setData(tempData.sort((a, b) => {
-      if (b.wp_pool_rewards !== a.wp_pool_rewards) {
-        return b.wp_pool_rewards - a.wp_pool_rewards;
-      }
-      if (b.wp !== a.wp) {
-        return b.wp - a.wp;
-      }
-      return b.fated_pool_rewards - a.fated_pool_rewards;
-    }));
+
+    setData(tempData.sort(sortWisherData));
   }, [cycle])
+
+  const sortWisherData = (a: WisherData, b: WisherData): number => {
+    if (b.fated_pool_rewards !== a.fated_pool_rewards) {
+      return b.fated_pool_rewards - a.fated_pool_rewards;
+    }
+    if (b.wp_pool_rewards !== a.wp_pool_rewards) {
+      return b.wp_pool_rewards - a.wp_pool_rewards;
+    }
+    if (b.fated_pool_qualified !== a.fated_pool_qualified) {
+      return Number(b.fated_pool_qualified) - Number(a.fated_pool_qualified);
+    }
+    return b.wp - a.wp;
+  };
+
+  const getStarWisherArr = (): string[] => {
+    const cycleInfo = getCycleInfo(cycle, 2);
+    if (!cycleInfo || (Number(cycleInfo.wisherCount) === 0 && cycleInfo.isboost)) {
+      return [];
+    }
+
+    const indexId = Number(cycleInfo.wisherIndexId);
+    const res: string[] = [];
+
+    for (let index = 1; index <= cycleInfo.wisherCount; index++) {
+      const wisherInfo = getWisherByIndex(2, indexId, index);
+      if (wisherInfo) {
+        res.push(wisherInfo.wisher);
+      }
+    }
+
+    return res;
+  };
+
 
   return (
     <div className={styles.overlay}>
@@ -68,12 +109,12 @@ const Selected = ({ cycle, onClose }: Props) => {
 
         <div className={styles.tableWrapper}>
           <div className={styles.tableHeader}>
-            <div className={`${styles.cell} ${styles.colNum}`}>Num</div>
-            <div className={`${styles.cell} ${styles.colAddress}`}>Address</div>
-            <div className={`${styles.cell} ${styles.colWishPoints}`}>Wish Points</div>
-            <div className={`${styles.cell} ${styles.colWishPointsPool}`}>Wish Points Pool {CURRENCY_SYMBOL}</div>
-            <div className={`${styles.cell} ${styles.colFatedPoolQualified}`}>Fated Pool Qualified</div>
-            <div className={`${styles.cell} ${styles.colFatedPool}`}>Fated Pool {CURRENCY_SYMBOL}</div>
+            <div className={`${styles.colNum}`}>Num</div>
+            <div className={`${styles.colAddress}`}>Address</div>
+            <div className={`${styles.colWishPoints}`}>Wish Points</div>
+            <div className={`${styles.colWishPointsPool}`}>Wish Points Pool {CURRENCY_SYMBOL}</div>
+            <div className={`${styles.colFatedPoolQualified}`}>Fated Pool Qualified</div>
+            <div className={`${styles.colFatedPool}`} style={{borderRight: "1px solid rgba(255, 209, 98, 1)"}}>Fated Pool {CURRENCY_SYMBOL}</div>
           </div>
           <div className={styles.scrollContainer}>
             <div className={styles.tableBodyWrapper}>
@@ -87,7 +128,7 @@ const Selected = ({ cycle, onClose }: Props) => {
                     </div>
                     <div className={`${styles.cell} ${styles.colWishPoints}`}>{row.wp} WP</div>
                     <div className={`${styles.cell} ${styles.colWishPointsPool}`}>{row.wp_pool_rewards} {CURRENCY_SYMBOL}</div>
-                    <div className={`${styles.cell} ${styles.colFatedPoolQualified}`}>{row.wp_pool_rewards ? 'Yes' : 'No'}</div>
+                    <div className={`${styles.cell} ${styles.colFatedPoolQualified}`}>{row.fated_pool_qualified ? 'Yes' : 'No'}</div>
                     <div className={`${styles.cell} ${styles.colFatedPool}`}>{row.fated_pool_rewards} {CURRENCY_SYMBOL}</div>
                   </div>
                 ))}
