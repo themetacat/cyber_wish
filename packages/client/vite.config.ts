bb970faf0cebc68bc84212afc32357bac708dbf9
MJumpKing <2572430586@qq.com>
2025-06-16 17:20:19 +0800

    fix: deploy to vercel path 404

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { mud } from "vite-plugin-mud";
 
 export default defineConfig({
+  base: "/",
   plugins: [react(), mud({ worldsFile: "../contracts/worlds.json" })],
   build: {
     target: "es2022",
